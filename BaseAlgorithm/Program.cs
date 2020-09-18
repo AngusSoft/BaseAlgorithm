656c0a281f103399f88693c53580bc7b39dedcb5
fuzhenfeng <fuzhenfeng@zhijiaxing.net>
2020-09-18 11:05:34 +0800

    冒泡算法

---
+++ BaseAlgorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿using SortAlgorithm;
+using System;
+
+namespace BaseAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 15, 20, 7, 9, 24, 3};
+            SortAlgorithm(array);
+            Console.ReadKey();
+        }
+
+        static void SortAlgorithm(int[] array)
+        {
+            BubbleSort.Sort(array);
+            Console.WriteLine($"冒泡排序结果：{string.Join(",", array)}");
+        }
+    }
+}
---
+++ SortAlgorithm/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SortAlgorithm
+{
+    /// <summary>
+    /// 冒泡排序
+    /// </summary>
+    public class BubbleSort
+    {
+        public static void Sort(int[] array)
+        {
+            for(int i = 0; i < array.Length - 1; i++)
+            {
+                bool flag = false;
+                for(int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if(array[j] > array[j + 1])
+                    {
+                        var temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+                if (!flag) break;
+            }
+        }
+    }
+}
